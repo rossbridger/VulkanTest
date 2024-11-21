2fb56ea6c5702a99d0a1a4a80634ade18482c5ed
Ross Charles Campbell <10781734+rossbridger@users.noreply.github.com>
2024-11-21 20:09:54 +0800

    First workable version.
    Signed-off-by: Ross Charles Campbell <10781734+rossbridger@users.noreply.github.com>

--- vulkanrenderer.h
+++ vulkanrenderer.h
@@ -16,6 +16,22 @@ public:
 private:
 	QVulkanWindow *m_window;
 	QVulkanDeviceFunctions *m_devFuncs;
+
+	VkDeviceMemory m_bufMem = VK_NULL_HANDLE;
+	VkBuffer m_buf = VK_NULL_HANDLE;
+	VkDescriptorBufferInfo m_uniformBufInfo[QVulkanWindow::MAX_CONCURRENT_FRAME_COUNT];
+
+	VkDescriptorPool m_descPool = VK_NULL_HANDLE;
+	VkDescriptorSetLayout m_descSetLayout = VK_NULL_HANDLE;
+	VkDescriptorSet m_descSet[QVulkanWindow::MAX_CONCURRENT_FRAME_COUNT];
+
+	VkPipelineCache m_pipelineCache = VK_NULL_HANDLE;
+	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+	VkPipeline m_pipeline = VK_NULL_HANDLE;
+
+	QMatrix4x4 m_proj;
+	float m_rotation = 0.0f;
+	VkShaderModule createShader(const QString &name);
 };
 
 #endif // VULKANRENDERER_H
